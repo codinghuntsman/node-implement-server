f1d373937ec71706ff7611a811bc7477cd6d813a
Sajedul-Islam <sajedulislam.cse@gmail.com>
2022-09-23 11:42:26 +0600

    some steps added

---
+++ _steps.js
@@ -0,0 +1,10 @@
+/**
+ ** ExpressJS framework Installation > or go to ExpressJS website
+ * 1. npm init
+ * 2. npm install express
+ * 
+ ** Require cross origin Middleware
+ * 1. const cors = require('cors');
+ * 2. app.use(cors());
+ * 
+*/
